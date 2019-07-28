386beed4285473cba3ba6b05f642cf8f21ce1399
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-07-28 10:24:12 -0700

    area

--- views/requat-job/index.php
+++ views/requat-job/index.php
@@ -6,72 +6,62 @@ use conquer\select2\Select2Widget;
 use yii\helpers\ArrayHelper;
 use app\models\Nationality;
 use app\models\Governorate;
-
-/* @var $this yii\web\View */
-/* @var $model app\models\RequastJob */
-/* @var $form yii\widgets\ActiveForm */
+use app\models\Area;
 ?>
 
 <div class="requast-job-form">
 
-        <div class="panel panel-default">
-            <div class="panel-heading">sssss</div>
-            <div class="panel-body">
+    <div class="panel panel-default">
+        <div class="panel-heading">sssss</div>
+        <div class="panel-body">
             <?php $form = ActiveForm::begin(); ?>
-                <div class="row">
-                    <div class="col-md-6">
-                      <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-                      <?= $form->field($model, 'agree')->textInput() ?>
-                      <?= $form->field($model, 'phone')->textInput() ?>
-                        <?=  $form->field($model, 'nationality')->widget(
-                                Select2Widget::className(),
-                                    [
-                                        'items'=>ArrayHelper::map(Nationality::find()->all(), 'id', 'name_ar')
-                                    ]
-                                );?>
+            <div class="row">
+                <div class="col-md-6">
+                    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($model, 'agree')->textInput() ?>
+                    <?= $form->field($model, 'phone')->textInput() ?>
+                    <?= $form->field($model, 'nationality')->widget(
+                        Select2Widget::className(),
+                        [
+                            'items' => ArrayHelper::map(Nationality::find()->all(), 'id', 'name_ar')
+                        ]
+                    ); ?>
 
-                <?=  $form->field($model, 'governorate')->widget(
-                                                Select2Widget::className(),
-                                                    [
-                                                        'items'=>ArrayHelper::map(Governorate::find()->all(), 'id', 'name_ar')
-                                                    ]
-                                                );?>
-                          <?= $form->field($model, 'area')->textInput() ?>
+                    <?= $form->field($model, 'governorate')->widget(
+                        Select2Widget::className(),
+                        [
+                            'items' => ArrayHelper::map(Governorate::find()->all(), 'id', 'name_ar')
+                        ]
+                    ); ?>
 
-<?= $form->field($model, 'expected_salary')->textInput() ?>
+                    <?= $form->field($model, 'area')->widget(
+                        Select2Widget::className(),
+                        [
+                            'items' => ArrayHelper::map(Area::find()->all(), 'name_ar', 'name_ar')
+                        ]
+                    ); ?>
+                   
+
+                    <?= $form->field($model, 'expected_salary')->textInput() ?>
 
 
-                    </div>
-                    <div class="col-md-6">
+                </div>
+                <div class="col-md-6">
                     <?= $form->field($model, 'certificates')->textarea(['rows' => 6]) ?>
 
                     <?= $form->field($model, 'experience')->textarea(['rows' => 6]) ?>
 
-                 
-                    </div>
+
                 </div>
-                <div class="row">
-                <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
-    </div>
-                </div>
-                <?php ActiveForm::end(); ?>
             </div>
+            <div class="row">
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+                </div>
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
-        </div>
-   
-
-    
-
-  
-
-  
-
- 
-
-
-
-
-
+    </div>
+</div>
 
 </div>
