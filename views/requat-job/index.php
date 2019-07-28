23b9a02d8f3197ae82830868ab65cfcb56104d1e
hasan <hassanhitleap1@gmail.com>
2019-07-28 15:38:06 +0300

    requast

--- views/requat-job/index.php
+++ views/requat-job/index.php
@@ -10,29 +10,50 @@ use yii\widgets\ActiveForm;
 
 <div class="requast-job-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">sssss</div>
+            <div class="panel-body">
+            <?php $form = ActiveForm::begin(); ?>
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+                      <?= $form->field($model, 'agree')->textInput() ?>
+                      <?= $form->field($model, 'phone')->textInput() ?>
+                        <?= $form->field($model, 'nationality')->textInput() ?>
+                        <?= $form->field($model, 'governorate')->textInput() ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+<?= $form->field($model, 'expected_salary')->textInput() ?>
+                    </div>
+                    <div class="col-md-6">
+                    <?= $form->field($model, 'certificates')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'agree')->textInput() ?>
+                    <?= $form->field($model, 'experience')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'phone')->textInput() ?>
-
-    <?= $form->field($model, 'nationality')->textInput() ?>
-
-    <?= $form->field($model, 'certificates')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'experience')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'governorate')->textInput() ?>
-
-    <?= $form->field($model, 'expected_salary')->textInput() ?>
-
-
-    <div class="form-group">
+                 
+                    </div>
+                </div>
+                <div class="row">
+                <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
+                </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+        </div>
+   
 
-    <?php ActiveForm::end(); ?>
+    
+
+  
+
+  
+
+ 
+
+
+
+
+
 
 </div>
