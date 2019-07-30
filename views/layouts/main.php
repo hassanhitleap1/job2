1d538b96a8a131269d03eecdded73318260dfd16
hasan <hassanhitleap1@gmail.com>
2019-07-30 17:32:12 +0300

    ddd

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -75,10 +75,9 @@ AppAsset::register($this);
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
+        <?= $content ?>        
     </div>
-    <div class="container">
-    <?= $content ?>
-    </div>
+
 </div>
 
 <footer class="footer">
