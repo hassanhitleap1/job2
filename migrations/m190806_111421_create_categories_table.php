b2f7ea4f345f03b436ea1f8ec55afab765347051
hasan <hassanhitleap1@gmail.com>
2019-08-07 17:32:33 +0300

    a

--- migrations/m190806_111421_create_categories_table.php
+++ migrations/m190806_111421_create_categories_table.php
@@ -7,6 +7,22 @@ use yii\db\Migration;
  */
 class m190806_111421_create_categories_table extends Migration
 {
+    
+    public $data = [
+        ['name_ar' => 'وظائف فندقة مطاعم'],
+        ['name_ar' => 'وظائف فنيين وحرفين'],
+        ['name_ar' => 'وظائف صحه وجمال'],
+        ['name_ar' => 'وظائف مبيعات'],
+        ['name_ar' => 'وظائف تدريس ودورات تدريب'],
+        ['name_ar' => 'وظائف سائقين وتوصيل'],
+        ['name_ar' => 'وظائف طب تمريض صيدلة'],
+        ['name_ar' => 'وظائف  اداره سكراريا'],
+        ['name_ar' => 'وظائف تسويق'],
+        ['name_ar' => 'تصميم'],
+        ['name_ar' => 'محاسبه ومالية'],
+        ['name_ar' => 'هندسة'],
+        ['name_ar' => 'مكانيك سيارات']
+    ];
     /**
      * {@inheritdoc}
      */
