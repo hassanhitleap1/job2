0d0616c141c162d0fa2fd4532a4fd44eee36912d
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-08-18 15:28:02 -0700

    s

--- components/SMSCompnent.php
+++ components/SMSCompnent.php
@@ -16,33 +16,18 @@ class SMSCompnent extends Component
     }
 
     public function sendsms(){
-       $client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(self::API_KEY, self::  API_SECRET));
-        $client = new Nexmo\Client(
-          new Nexmo\Client\Credentials\Basic(self::API_KEY, self:: API_SECRET),
-          [
-            'base_api_url' => 'https://example.com'
-          ]
-        );
 
-    try {
-      $message = $client->message()->send([
-        'to' => TO_NUMBER,
-        'from' => 'Acme Inc',
-        'text' => 'A text message sent using the Nexmo SMS API'
-      ]);
-      $response = $message->getResponseData();
+    $basic  = new \Nexmo\Client\Credentials\Basic('cf4a8d89', 'qX9SFGqhhGhf7p1e');
+    $client = new \Nexmo\Client($basic);
 
-      if ($response['messages'][0]['status'] == 0) {
-        echo "The message was sent successfully\n";
-      } else {
-        echo "The message failed with status: " . $response['messages'][0]['status'] . "\n";
-      }
-    } catch (Exception $e) {
-      echo "The message was not sent. Error: " . $e->getMessage() . "\n";
-    }
+    $message = $client->message()->send([
+      'to' => '962799263494',
+      'from' => 'Nexmo',
+      'text' => 'Hello from Nexmo'
+    ]);
 
-        
  
+
 
     }
 
