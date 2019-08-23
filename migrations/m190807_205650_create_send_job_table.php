d8d873959dca402908f1df47188e8e1f3b993031
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-08-23 10:44:57 -0700

    s

--- migrations/m190807_205650_create_send_job_table.php
+++ migrations/m190807_205650_create_send_job_table.php
@@ -12,11 +12,16 @@ class m190807_205650_create_send_job_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%send_job}}', [
             'id' => $this->primaryKey(),
             'title'=>$this->string(),
             'body' => $this->text(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
