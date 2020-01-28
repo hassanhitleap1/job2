2cc57b36770414a31baba06f1ca79072138e18a8
hassan kiwan <hassanhitleap1@gmail.com>
2020-01-28 16:11:37 +0200

    cv

--- views/requast-job/show_cv.php
+++ views/requast-job/show_cv.php
@@ -51,16 +51,16 @@ $this->params['breadcrumbs'][] = $this->title;
                         <div class="col-md-12">
                             <div class="progress-wrapper">
                                 <div class="progress-item">
-                                    <span class="progress-title">Marketing</span>
+                                    <span class="progress-title">قدره تحمل العمل </span>
                                     <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="62" aria-valuemin="0" aria-valuemax="100" style="width: 62%"><span class="progress-percent"> 62%</span>
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 62%"><span class="progress-percent"> 90%</span>
                                         </div>
                                     </div>
 
                                 </div>
 
                                 <div class="progress-item">
-                                    <span class="progress-title">Market Research</span>
+                                    <span class="progress-title"> العمل بروح الفريق</span>
                                     <div class="progress">
                                         <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%"><span class="progress-percent"> 90%</span>
                                         </div>
@@ -78,18 +78,18 @@ $this->params['breadcrumbs'][] = $this->title;
                                 </div>
 
                                 <div class="progress-item">
-                                    <span class="progress-title">Communtcation Skills</span>
+                                    <span class="progress-title">الالتزام الرسمي بالدوام</span>
                                     <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%;"><span class="progress-percent"> 55%</span>
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="87" aria-valuemin="0" aria-valuemax="100" style="width: 55%;"><span class="progress-percent"> 87%</span>
                                         </div>
                                     </div>
 
                                 </div>
 
                                 <div class="progress-item">
-                                    <span class="progress-title">Project Management</span>
+                                    <span class="progress-title">مهارات التواصل </span>
                                     <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"><span class="progress-percent"> 80%</span>
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"><span class="progress-percent"> 85%</span>
                                         </div>
                                     </div>
 
@@ -112,7 +112,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="row">
                         <div class="col-md-12">
                             <div class="section-title">
-                                <h2>Expertise</h2>
+                                <h2>عني</h2>
                             </div>
                         </div>
                     </div>
@@ -120,38 +120,24 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="row">
                         <div class="col-md-6">
                             <div class="expertise-item">
-                                <h3>Professionally drive</h3>
+                                <h3>الشهادات</h3>
                                 <p>
-                                    Synergistically strategize customer directed resources rather than principle.
+                                    <?=$model->certificates?>
                                 </p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="expertise-item">
-                                <h3>Seamlessly leverage </h3>
+                                <h3>الخبرات</h3>
                                 <p>
-                                    Quickly repurpose reliable customer service with orthogonal ideas. Competently.
+                                    <?=$model->experience?>
                                 </p>
                             </div>
                         </div>
+
+
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="expertise-item">
-                                <h3>Interactively incubate</h3>
-                                <p>
-                                    Interactively myocardinate high standards in initiatives rather than next-generation.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="expertise-item">
-                                <h3>Globally streamline</h3>
-                                <p>
-                                    Dynamically initiate client-based convergence vis-a-vis performance based. </p>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             </section>
 
@@ -265,9 +251,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
             </section>
 
-        
-
-
         </div>
 
     </div>
@@ -275,124 +258,11 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 </div>
-
-<div class="container" id="cv-print">
-    <div class="row">
-        <div class="name-cv col-lg-4 col-md-4 col-offset-3">
-            <?=Html::img('@web/images/logo.svg', ['class' => 'logo ']) ?>
-            <h3> جرس  : السيرة الذاتية </h3>
-        </div>
+<div class="row">
+    <div class="name-cv col-lg-4 col-md-4 col-offset-3">
+        <?=Html::img('@web/images/logo.svg', ['class' => 'logo ']) ?>
+        <h3> جرس  : السيرة الذاتية </h3>
     </div>
-    <hr />
-   
-
-    <div class="row">
-        <div class="col-md-5 column-info">
-           
-            <div class="row">
-                <div class="col-md-12">
-                    <?php if ($model->avatar == null): ?>
-                            <?=Html::img('@web/images/profile.png', ['class' => 'image-profile-cv']) ?>
-                    <?php
-else: ?>
-                            <?=Html::img("@web/$model->avatar", ['class' => 'image-profile-cv']) ?>
-                    <?php
-endif; ?>
-                </div>    
-            </div>
-            <div class="row info-profile">
-                
-                <h3><span class="glyphicon glyphicon-user"> </span> معلومات الشخصية :</h3>
-               
-                <hr>
-                <div class="cal-md-12">
-                    
-                    <ul style="font-size: 20px;">
-                        <li> هاتف :- <?=$model->phone ?> </li>
-                        <li> العمر:- <?=$model->agree ?> سنين </li>
-                        <li> الجنسية :- <?=$model->nationality0->name_ar ?> </li>
-                        <li> الجنس :- <?=($model->gender == 1) ? 'ذكر' : ($model->gender == 2) ? 'انثى' : 'غير محدد'; ?> </li>
-                    </ul>
-                </div>
-            </div>
-            <hr>
-            <div class="row info-profile">
-            <h3><span class="glyphicon glyphicon-stats"> </span> المهارات الشخصية  :</h3>
-                
-                <div class="cal-md-12">
-                  
-                    <div>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-custum" role="progressbar" aria-valuenow="98"
-                            aria-valuemin="0" aria-valuemax="100" style="width: 98%;" >
-                                    98% قدره تحمل العمل 
-                            </div>
-                        </div>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-custum" role="progressbar" aria-valuenow="97"
-                            aria-valuemin="0" aria-valuemax="100"  style="width: 97%;">
-                                97% العمل بروح الفريق
-                            </div>
-                        </div>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-custum" role="progressbar" aria-valuenow="100"
-                            aria-valuemin="0" aria-valuemax="100" style="width: 95%;">
-                                 95% الالتزام الرسمي بالدوام 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>  
-        <div class="vl"></div>
-        <div class="col-md-7 row2">
-            <div class="row">
-                    <div class="panel card-name">
-                        <div class="panel-heading">
-                            <h2>
-                            الاسم :- <?=$model->name ?>
-                            <h2>
-                        </div>
-                    </div>   
-            </div>
-            
-            <div class="row">
-                    <h3><span class="glyphicon glyphicon-wrench blue-h"></span><strong class="blue-h"> الشهادات :</strong></h3>
-                    <hr>
-                    <div class="col-md-12">
-                        
-                        <p style="font-size: 20px;">
-                            <?=$model->certificates
-?>
-                        </p>
-                    </div>
-            </div>
-            <hr />
-        <div class="row">
-            <h3><span class="glyphicon glyphicon-book   blue-h"></span> <strong class="blue-h"> الخبرات :</strong> </h3>
-            <hr>
-            <div class="col-md-12">
-                <p style="font-size: 20px;">
-                    <?=$model->experience
-?>
-                </p>
-            </div>
-        </div>
-
-        </div>
-        <hr />
-        <div class="row note">
-            <div class="col-md-12">
-                <p style="margin-top: 40px;">
-                   <span class="glyphicon glyphicon-star-empty"></span>
-                الموظف الذي يحمل هذه السيره الذاتية يرجى التوصيه به بنأ على الوصف والمواصفات الوظيفية ويرجى تزويدنا بتاريخ التوظيف
-
-                </p>
-            </div>
-        </div>
-             
-    </div>        
-
 </div>
 
 
