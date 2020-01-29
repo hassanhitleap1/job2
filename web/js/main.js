d940f0e832d24ede2dc64ee2befaef95e7da1c66
hassan kiwan <hassanhitleap1@gmail.com>
2020-01-29 13:00:09 +0200

    print

--- web/js/main.js
+++ web/js/main.js
@@ -25,7 +25,8 @@ $(document).ready(function () {
 
 
 $(document).on('click', '#print_cv', function () {
-    chart_div();
+    printDiv();
+    //chart_div();
 });
 
 
@@ -33,11 +34,11 @@ function chart_div()
 {
 
     var styelink='';
-    var SITE_URL= window.location.origin+'/web';
-    styelink='<link href="'+SITE_URL+'/assets/b518d80/css/bootstrap.css" media="print" rel="stylesheet">';
-    styelink+='<link href="'+SITE_URL+'/assets/6768feb/css/bootstrap-rtl.css" media="print" rel="stylesheet">';
-    styelink+='<link href="'+SITE_URL+'/resumex/css/font-awesome.min.css" media="print" rel="stylesheet">';
-    styelink+='<link href="'+SITE_URL+'/resumex/css/style.css"  media="print" rel="stylesheet">';
+    var SITE_URL= window.location.origin+'';
+    styelink='<link href="'+SITE_URL+'/assets/b518d80/css/bootstrap.css"  rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/assets/6768feb/css/bootstrap-rtl.css"  rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/resumex/css/font-awesome.min.css" rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/resumex/css/style.css"  rel="stylesheet">';
     styelink+='<style>.theiaStickySidebar:after {content: ""; display: table; clear: both;}</style>';
     var divToPrint=document.getElementById("cv_div");
     newWin= window.open("");
@@ -48,5 +49,31 @@ function chart_div()
     newWin.document.write('</body></html>');
     newWin.print();
     // newWin.close();
+}
+
+function printDiv()
+{
+    var styelink='';
+    var SITE_URL= window.location.origin+'';
+    styelink='<link href="'+SITE_URL+'/assets/b518d80/css/bootstrap.css"  rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/assets/6768feb/css/bootstrap-rtl.css"  rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/resumex/css/font-awesome.min.css" rel="stylesheet">';
+    styelink+='<link href="'+SITE_URL+'/resumex/css/style.css"  rel="stylesheet">';
+    styelink+='<style>.theiaStickySidebar:after {content: ""; display: table; clear: both;}</style>';
+    // var a9= parseInt($(".swiper-pagination-total").html())+1;
+    // var b9= $(window).height();
+    // var b91= $(window).height();
+    // $(".questions-main-container .content-container").height(a9*b9);
+    // $(".result-page-container").height($(window).height());
+    var divToPrint=document.getElementById('cv_div');
+    //var divToPrint1=document.getElementById('print-result-a');
+    var newWin=window.open('','Print-Window');
+    newWin.document.open();
+    newWin.document.write('<html><head><link href="'+SITE_URL+'/assets/b518d80/css/bootstrap.css"  rel="stylesheet"><link href="'+SITE_URL+'/assets/6768feb/css/bootstrap-rtl.css"  rel="stylesheet"><link href="'+SITE_URL+'/resumex/css/font-awesome.min.css" rel="stylesheet"><link href="'+SITE_URL+'/resumex/css/style.css"  rel="stylesheet"></head><body onload="window.print()">'+ divToPrint.innerHTML+'</body></html>');
+    newWin.document.close();
+    setTimeout(function(){
+        newWin.close();
+       // $(".questions-main-container").addClass("zoomOut").removeClass("animated zoomIn").fadeOut();
+    },10);
 }
 
