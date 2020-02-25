bc3b55615ee90e34f013cf43fec0b36228ca14ad
hasan kiwan <hassankiwan92@gmail.com>
2020-02-26 00:21:36 +0200

    123

--- controllers/RequastJobGoogleController.php
+++ controllers/RequastJobGoogleController.php
@@ -11,6 +11,7 @@ use app\models\RequastJobNotPaySearch;
 use app\models\SendSmsModel;
 use app\models\User;
 use Carbon\Carbon;
+use Google_Service_Sheets;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\UploadedFile;
@@ -134,9 +135,7 @@ class RequastJobGoogleController extends BaseController
 
 
 
-    public function actionImportFromGoogle(){
-        return "sss";
-    }
+
     /**
      * Finds the RequastJob model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
@@ -155,5 +154,78 @@ class RequastJobGoogleController extends BaseController
 
 
 
+    public function actionImportFromGoogle(){
+        $client=$this->getClient();
+        $service = new Google_Service_Sheets($client);
+
+        // Prints the names and majors of students in a sample spreadsheet:
+        // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+        $spreadsheetId = '1eOHxqu0RCB-wDCkWyxkPMHKpMs338VDuxoACzy-8TJM';
+        $range = 'Class Data!A2:E';
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+
+        if (empty($values)) {
+            print "No data found.\n";
+        } else {
+            print "Name, Major:\n";
+            foreach ($values as $row) {
+                // Print columns A and E, which correspond to indices 0 and 4.
+                printf("%s, %s\n", $row[0], $row[4]);
+            }
+        }
+            return "sss";
+        }
+    
+
+  private  function getClient()
+{
+    $client = new \Google_Client();
+    $client->setApplicationName('quickstart');
+    $client->setScopes(\Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+    die('');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
     
 }
