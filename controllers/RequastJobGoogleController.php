3f7afbfd0f3bf36031324f1c5ecc22e3560a4bfb
hasan kiwan <hassankiwan92@gmail.com>
2020-03-12 01:13:18 +0200

    ,

--- controllers/RequastJobGoogleController.php
+++ controllers/RequastJobGoogleController.php
@@ -7,6 +7,7 @@ use app\models\RequastJobGoogle;
 use app\models\RequastJobGoogleSearch;
 use Yii;
 use app\models\RequastJob;
+use app\models\RequastJobNotPay;
 use app\models\RequastJobNotPaySearch;
 use app\models\SendSmsModel;
 use app\models\User;
@@ -119,19 +120,6 @@ class RequastJobGoogleController extends BaseController
 
 
 
-        /**
-     * Displays a single RequastJob model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionShowCv($id)
-    {
-        $this->layout = "cv-layout";
-        return $this->render('show_cv', [
-            'model' => $this->findModel($id),
-        ]);
-    }
 
 
 
@@ -153,79 +141,37 @@ class RequastJobGoogleController extends BaseController
     }
 
 
+       /**
+     * Deletes an existing RequastJob model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionMoveModel($id)
+    {
+       $model= $this->findModel($id);
+       
+       $modelForm= new RequastJobNotPay();
+       $modelForm->name=$model->name;
+       $modelForm->agree=$model->agree;
+       $modelForm->gender=$_POST['radio'];
+       $modelForm->phone=$model->phone;
+       $modelForm->nationality=$model->nationality;
+       $modelForm->certificates=$model->certificates;
+       $modelForm->experience=$model->experience;
+       $modelForm->governorate=$model->governorate;
+       $modelForm->area=$model->area;
+       $modelForm->expected_salary=$model->expected_salary;
+       $modelForm->save(false);
+              
 
-    public function actionImportFromGoogle(){
-        $client=$this->getClient();
-        $service = new Google_Service_Sheets($client);
-
-        // Prints the names and majors of students in a sample spreadsheet:
-        // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-        $spreadsheetId = '1eOHxqu0RCB-wDCkWyxkPMHKpMs338VDuxoACzy-8TJM';
-        $range = 'Class Data!A2:E';
-        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-        $values = $response->getValues();
-
-        if (empty($values)) {
-            print "No data found.\n";
-        } else {
-            print "Name, Major:\n";
-            foreach ($values as $row) {
-                // Print columns A and E, which correspond to indices 0 and 4.
-                printf("%s, %s\n", $row[0], $row[4]);
-            }
-        }
-            return "sss";
-        }
-    
-
-  private  function getClient()
-{
-    $client = new \Google_Client();
-    $client->setApplicationName('quickstart');
-    $client->setScopes(\Google_Service_Sheets::SPREADSHEETS_READONLY);
-    $client->setAuthConfig('credentials.json');
-    $client->setAccessType('offline');
-    $client->setPrompt('select_account consent');
-    die('');
-
-    // Load previously authorized token from a file, if it exists.
-    // The file token.json stores the user's access and refresh tokens, and is
-    // created automatically when the authorization flow completes for the first
-    // time.
-    $tokenPath = 'token.json';
-    if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        $client->setAccessToken($accessToken);
+       $modelCountSendSms = new CountSendSms();
+       $modelCountSendSms->user_id=$modelForm->id;
+       $modelCountSendSms->count=0;
+       $modelCountSendSms->save(false);
+       Yii::$app->session->setFlash('message', 'تم ارسال نموذج التقديم لوظيفة');
+       
+        return $this->redirect(['index']);
     }
-
-    // If there is no previous token or it's expired.
-    if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
-        if ($client->getRefreshToken()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        } else {
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
-        }
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
-}
-    
 }
--- controllers/RequatJobController.php
+++ controllers/RequatJobController.php
@@ -3,7 +3,9 @@
 namespace app\controllers;
 
 use app\models\CountSendSms;
+use app\models\RequastJob;
 use app\models\RequastJobGoogle;
+use app\models\RequastJobNotPay;
 use Yii;
 use yii\web\Controller;
 use app\models\RequastJobVisitor;
@@ -35,12 +37,13 @@ class RequatJobController extends \yii\web\Controller
                 $modelForm->governorate=$model->governorate;
                 $modelForm->area=$model->area;
                 $modelForm->expected_salary=$model->expected_salary;
+
                 if (!is_null($file)) {
                     $imagename = 'images/avatar/' . md5(uniqid(rand(), true)) . '.' . $file->extension;
                     $file->saveAs($imagename);
                     $modelForm->avatar= $imagename;
                 }
-                $id =Yii::$app->db->createCommand('SELECT id FROM user_from_google ORDER BY id DESC LIMIT 1')
+                $id =Yii::$app->db->createCommand('SELECT id FROM 	user_from_google  ORDER BY id DESC LIMIT 1')
                     ->queryScalar();
                 if($id==''){
                     $id=1;
@@ -65,7 +68,7 @@ class RequatJobController extends \yii\web\Controller
                 $modelCountSendSms->user_id=$modelForm->id;
                 $modelCountSendSms->count=0;
                 $modelCountSendSms->save(false);
-                Yii::$app->session->setFlash('message', 'send aplication sucessfuly');
+                Yii::$app->session->setFlash('message', 'تم ارسال نموذج التقديم لوظيفة');
                 
                 return $this->render('index', [
                     'model' => $model,
@@ -81,56 +84,5 @@ class RequatJobController extends \yii\web\Controller
     }
 
 
-    /*
-     if ($model->load(Yii::$app->request->post()) ) {
-            $file = UploadedFile::getInstance($model, 'avatar');
-            $cv = UploadedFile::getInstance($model, 'cv');
-            if ($model->validate()) {
-                 $data['name']=$model->name;
-                 $data['agree']=$model->agree;
-                  $data['phone']=$model->phone;
-                  $data['nationality']=$model->nationality;
-                  $data['certificates']=$model->certificates;
-                  $data['experience']=$model->experience;
-                  $data['governorate']=$model->governorate;
-                  $data['area']=$model->area;
-                  $data['expected_salary']=$model->expected_salary;
-
-
-                if (!is_null($file)) {
-                    $imagename = 'images/avatar/' . md5(uniqid(rand(), true)) . '.' . $file->extension;
-                    $file->saveAs($imagename);
-                    $data["avatar"] = $imagename;
-                }
-
-                $id =Yii::$app->db->createCommand('SELECT id FROM user_from_google ORDER BY id DESC LIMIT 1')
-                    ->queryScalar();
-
-                if (!is_null($cv)) {
-                    if(is_dir("cv_form/$id")){
-                        rmdir("cv_form/$id");
-                    }else{
-                        mkdir("cv_form/$id");
-                    }
-                    $cvfullpath = "cv_form/$id/index" . '.' . $file->extension;
-                    $file->saveAs($cvfullpath);
-                }
-
-                Yii::$app->session->setFlash('success', 'send aplication sucessfuly');
-                Yii::$app->db
-                ->createCommand()
-                ->batchInsert('user_from_google', ['name','agree', 'phone','nationality','certificates','experience','governorate','expected_salary'],[$data])
-                ->execute();
-
-                return $this->render('index', [
-                    'model' => $model,
-                ]);
-                $modelCountSendSms = new CountSendSms();
-                $modelCountSendSms->user_id=$model->id;
-                $modelCountSendSms->count=0;
-                $modelCountSendSms->save(false);
-                return $this->goHome();
-            }
-        }
-     */
+  
 }
