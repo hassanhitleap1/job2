6165ec19b5beb3c3becab14f1745b082cd11fa9d
hassan kiwan <hassanhitleap1@gmail.com>
2020-05-29 22:09:34 +0300

    12

---
+++ views/requat-job/edu_att.php
@@ -0,0 +1,95 @@
+<?php
+
+use wbraganca\dynamicform\DynamicFormWidget;
+
+?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h4><i class="glyphicon glyphicon-envelope"></i><?= Yii::t('app', 'Educational_Attainment') ?> </h4>
+    </div>
+    <div class="panel-body">
+        <?php DynamicFormWidget::begin([
+            'widgetContainer' => 'dynamicform_wrapper_edu', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
+            'widgetBody' => '.container-items', // required: css class selector
+            'widgetItem' => '.item', // required: css class
+            'limit' => 4, // the maximum times, an element can be cloned (default 999)
+            'min' => 1, // 0 or 1 (default 1)
+            'insertButton' => '.add-item', // css class
+            'deleteButton' => '.remove-item', // css class
+            'model' => $modelsEducationalAttainment[0],
+            'formId' => 'dynamic-form',
+            'formFields' => [
+                'specialization',
+                'university',
+                'year_get',
+            ],
+        ]); ?>
+
+
+        <div class="container-items">
+            <!-- widgetContainer -->
+            <?php foreach ($modelsEducationalAttainment as $index => $modelEduAt) : ?>
+                <div class="item panel panel-default">
+                    <!-- widgetBody -->
+                    <div class="panel-heading">
+                        <h3 class="panel-title pull-left"><?= Yii::t('app', 'Request_Merchant') . " {" . ($index + 1) . "}" ?> </h3>
+                        <div class="pull-right">
+                            <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
+                            <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-body">
+                        <?php
+                        // necessary for update action.
+                        if (!$modelEduAt->isNewRecord) {
+                            echo Html::activeHiddenInput($modelEduAt, "[{$index}]id");
+                        }
+                        ?>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <?= $form->field($modelEduAt, "[{$index}]specialization")->textInput(['maxlength' => true]) ?>
+                            </div>
+                            <div class="col-md-4">
+                                <?= $form->field($modelEduAt, "[{$index}]university")->textInput(['maxlength' => true]) ?>
+                            </div>
+                            <div class="col-md-4">
+                                <?= $form->field($modelEduAt, "[{$index}]year_get")->textInput(['maxlength' => true]) ?>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <?php DynamicFormWidget::end(); ?>
+    </div>
+</div>
+
+<?php
+$js = '
+
+jQuery(".dynamicform_wrapper_edu").on("afterInsert", function(e, item) {
+   
+jQuery(".dynamicform_wrapper_edu .panel-title-address").each(function(index) {
+jQuery(this).html("Address: " + (index + 1))
+});
+});
+
+jQuery(".dynamicform_wrapper_edu").on("afterDelete", function(e) {
+jQuery(".dynamicform_wrapper_edu .panel-title-address").each(function(index) {
+jQuery(this).html("Address: " + (index + 1))
+});
+});
+
+$(".add-item").on("click",function(e){
+  governorate=$("#select2-merchant-governorate-container").val();
+  $("#requestmerchant-0-governorate").val(governorate);
+  area=$("select2-merchant-area-container").val();
+  $("select-0-requestmerchant-0-area-container").val(area);
+});
+
+';
+
+$this->registerJs($js);
+?>
--- views/requat-job/index.php
+++ views/requat-job/index.php
@@ -26,109 +26,51 @@ $this->title= Yii::t('app', 'Create_Requast_Job');
         <div class="panel-heading">
             <h1><?= Html::encode($this->title) ?></h1>
         </div>
-        <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
+            <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
             <div class="panel-body">
-            <div class="row">
-                <div class="col-lg-3">
-                    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-                </div>
-                <div class="col-lg-3">
-                    <?= $form->field($model, 'phone')->textInput() ?>
-                </div>
-                <div class="col-lg-3">
-                    <?= $form->field($model, "gender")->dropDownList([1 => "ذكر", 2 => "انثى"]); ?>
-                </div>
-                <div class="col-lg-3">
-                    <?= $form->field($model, 'agree')->textInput() ?>
-
-                    <?= $form->field($model, 'nationality')->widget(
-                        Select2Widget::className(),
-                        [
-                            'items' => ArrayHelper::map(Nationality::find()->all(), 'id', 'name_ar')
-                        ]
-                    ); ?>
-                </div>
-
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <?= $form->field($model, 'governorate')->widget(
-                        Select2Widget::className(),
-                        [
-                            'items' => ArrayHelper::map(Governorate::find()->all(), 'id', 'name_ar')
-                        ]
-                    ); ?>
-                </div>
-                <div class="col-md-6">
-                    <?= $form->field($model, 'area')->textInput() ?>
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4><i class="glyphicon glyphicon-envelope"></i><?= Yii::t('app', 'Create_Request_Merchant') ?> </h4>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
                     </div>
-                    <div class="panel-body">
-                        <?php DynamicFormWidget::begin([
-                            'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
-                            'widgetBody' => '.container-items', // required: css class selector
-                            'widgetItem' => '.item', // required: css class
-                            'limit' => 4, // the maximum times, an element can be cloned (default 999)
-                            'min' => 1, // 0 or 1 (default 1)
-                            'insertButton' => '.add-item', // css class
-                            'deleteButton' => '.remove-item', // css class
-                            'model' => $modelsEducationalAttainment[0],
-                            'formId' => 'dynamic-form',
-                            'formFields' => [
-                                'specialization',
-                                'university',
-                                'year_get',
-                            ],
-                        ]); ?>
+                    <div class="col-lg-3">
+                        <?= $form->field($model, 'phone')->textInput() ?>
+                    </div>
+                    <div class="col-lg-3">
+                        <?= $form->field($model, "gender")->dropDownList([1 => "ذكر", 2 => "انثى"]); ?>
+                    </div>
+                    <div class="col-lg-3">
+                        <?= $form->field($model, 'agree')->textInput() ?>
 
-
-                        <div class="container-items">
-                            <!-- widgetContainer -->
-                            <?php foreach ($modelsEducationalAttainment as $index => $modelEduAt) : ?>
-                                <div class="item panel panel-default">
-                                    <!-- widgetBody -->
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title pull-left"><?= Yii::t('app', 'Request_Merchant') . " {" . ($index + 1) . "}" ?> </h3>
-                                        <div class="pull-right">
-                                            <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
-                                            <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                    <div class="panel-body">
-                                        <?php
-                                        // necessary for update action.
-                                        if (!$modelEduAt->isNewRecord) {
-                                            echo Html::activeHiddenInput($modelEduAt, "[{$index}]id");
-                                        }
-                                        ?>
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <?= $form->field($modelEduAt, "[{$index}]specialization")->textInput(['maxlength' => true]) ?>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <?= $form->field($modelEduAt, "[{$index}]university")->textInput(['maxlength' => true]) ?>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <?= $form->field($modelEduAt, "[{$index}]year_get")->textInput(['maxlength' => true]) ?>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                        <?php DynamicFormWidget::end(); ?>
+                        <?= $form->field($model, 'nationality')->widget(
+                            Select2Widget::className(),
+                            [
+                                'items' => ArrayHelper::map(Nationality::find()->all(), 'id', 'name_ar')
+                            ]
+                        ); ?>
                     </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'governorate')->widget(
+                            Select2Widget::className(),
+                            [
+                                'items' => ArrayHelper::map(Governorate::find()->all(), 'id', 'name_ar')
+                            ]
+                        ); ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'area')->textInput() ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <?php include('edu_att.php')?>
+                </div>
+                 <div class="row">
+                     <?php include('experience.php')?>
+                 </div>
+                 <div class="row">
+                     <?php include('courses.php')?>
+                 </div>
              <div class="row">
                  <div class="col-md-2">
                      <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary btn-lg ']) ?>
@@ -141,38 +83,9 @@ $this->title= Yii::t('app', 'Create_Requast_Job');
     <?php endif; ?>
 </div>
 
-<?php
-$js = '
 
-jQuery(".dynamicform_wrapper").on("afterInsert", function(e, item) {
-   
-jQuery(".dynamicform_wrapper .panel-title-address").each(function(index) {
-jQuery(this).html("Address: " + (index + 1))
-});
-});
 
-jQuery(".dynamicform_wrapper").on("afterDelete", function(e) {
-jQuery(".dynamicform_wrapper .panel-title-address").each(function(index) {
-jQuery(this).html("Address: " + (index + 1))
-});
-});
 
-$(".add-item").on("click",function(e){
-  governorate=$("#select2-merchant-governorate-container").val();
-  $("#requestmerchant-0-governorate").val(governorate);
-  area=$("select2-merchant-area-container").val();
-  $("select-0-requestmerchant-0-area-container").val(area);
-});
-
-';
-
-$this->registerJs($js);
-?>
-?>
-<?php// require('educational_attainment.php') ?>
-<?php //require('experience.php') ?>
-<?php // require('courses.php') ?>
-<?php // Yii::t('app', 'Courses') ?>
 
 
 
