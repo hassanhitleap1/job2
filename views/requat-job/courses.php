b3734c14ecad37c088862c34546859b020292f8a
hassanhitleap1 <hassanhitleap1@gmail.com>
2020-06-01 13:48:28 -0700

    mm

--- views/requat-job/courses.php
+++ views/requat-job/courses.php
@@ -1,73 +1,80 @@
 <?php
 
 use wbraganca\dynamicform\DynamicFormWidget; ?>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <?php DynamicFormWidget::begin([
+            'widgetContainer' => 'dynamicform_wrapper_courses', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
+            'widgetBody' => '.container-items', // required: css class selector
+            'widgetItem' => '.item', // required: css class
+            'limit' => 4, // the maximum times, an element can be cloned (default 999)
+            'min' => "0", // 0 or 1 (default 1)
+            'insertButton' => '.add-item', // css class
+            'deleteButton' => '.remove-item', // css class
+            'model' => $modelsCourses[0],
+            'formId' => 'dynamic-form',
+            'formFields' => [
+                'job_title',
+                'month_from_exp',
+                'year_from_exp',
+                'month_to_exp',
+                'year_to_exp',
+                'facility_name'
+            ],
+        ]); ?>
+
         <div class="panel panel-default">
-            <div class="panel-heading panel-hight">
+            <div class="panel-heading">
+                <i class="fa fa-envelope"></i>
                 <h4><?= Yii::t('app', 'Courses') ?> </h4>
+                <button type="button" class="pull-right add-item btn btn-success btn-xs"><i class="fa fa-plus"></i><?= Yii::t('app', 'Add') ?></button>
+                <div class="clearfix"></div>
             </div>
-
-            <div class="panel-body">
-                <?php DynamicFormWidget::begin([
-                    'widgetContainer' => 'dynamicform_wrapper_courses', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
-                    'widgetBody' => '.container-items', // required: css class selector
-                    'widgetItem' => '.item', // required: css class
-                    'limit' => 4, // the maximum times, an element can be cloned (default 999)
-                    'min' => 1, // 0 or 1 (default 1)
-                    'insertButton' => '.add-item', // css class
-                    'deleteButton' => '.remove-item', // css class
-                    'model' => $modelsCourses[0],
-                    'formId' => 'dynamic-form',
-                    'formFields' => [
-                        'job_title',
-                        'month_from_exp',
-                        'year_from_exp',
-                        'month_to_exp',
-                        'year_to_exp',
-                        'facility_name'
-                    ],
-                ]); ?>
-                <div class="container-items">
-                    <!-- widgetContainer -->
-                    <?php foreach ($modelsCourses as $index => $modelCourse) : ?>
-                        <div class="item panel panel-default">
-                            <!-- widgetBody -->
-                            <div class="panel-heading">
-                                <div class="pull-right">
-                                    <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
-                                    <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
-                                </div>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="panel-body">
-                                <?php
-                                if (!$modelCourse->isNewRecord) {
-                                    echo Html::activeHiddenInput($modelCourse, "[{$index}]id");
-                                }
-                                ?>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <?= $form->field($modelCourse, "[{$index}]name_course")->textInput(['maxlength' => true]) ?>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <?= $form->field($modelCourse, "[{$index}]destination")->textInput(['maxlength' => true]) ?>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <?= $form->field($modelCourse, "[{$index}]duration")->textInput(['maxlength' => true]) ?>
-                                    </div>
-                                </div>
-
-                            </div>
+            <div class="panel-body container-items">
+                <!-- widgetContainer -->
+                <?php foreach ($modelsCourses as $index => $modelCourse) : ?>
+                    <div class="item panel panel-default">
+                        <!-- widgetBody -->
+                        <div class="panel-heading">
+                            <span class="panel-title-address"></span>
+                            <button type="button" class="pull-right remove-item btn btn-danger btn-xs"><i class="fa fa-minus"></i></button>
+                            <div class="clearfix"></div>
                         </div>
-                    <?php endforeach; ?>
-                </div>
-                <?php DynamicFormWidget::end(); ?>
+                        <div class="panel-body">
+                            <?php
+                            // necessary for update action.
+                            if (!$modelCourse->isNewRecord) {
+                                echo Html::activeHiddenInput($modelCourse, "[{$index}]id");
+                            }
+                            ?>
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <?= $form->field($modelCourse, "[{$index}]name_course")->textInput(['maxlength' => true]) ?>
+                                </div>
+                                <div class="col-md-4">
+                                    <?= $form->field($modelCourse, "[{$index}]destination")->textInput(['maxlength' => true]) ?>
+                                </div>
+                                <div class="col-md-4">
+                                    <?= $form->field($modelCourse, "[{$index}]duration")->textInput(['maxlength' => true]) ?>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
 
+        <?php DynamicFormWidget::end(); ?>
+    </div>
+</div>
 
-    <?php
 
-    $js = '
+<?php
+
+$js = '
 
 jQuery(".dynamicform_wrapper_courses").on("afterInsert", function(e, item) {
    
@@ -91,5 +98,5 @@ $(".add-item").on("click",function(e){
 
 ';
 
-    $this->registerJs($js);
-    ?>
+$this->registerJs($js);
+?>
--- views/requat-job/edu_att.php
+++ views/requat-job/edu_att.php
@@ -6,16 +6,13 @@ $year = range(1990, date("Y"));
 
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading panel-hight">
-        <h4><?= Yii::t('app', 'Educational_Attainment') ?> </h4>
-    </div>
     <div class="panel-body">
         <?php DynamicFormWidget::begin([
             'widgetContainer' => 'dynamicform_wrapper_edu', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
             'widgetBody' => '.container-items', // required: css class selector
             'widgetItem' => '.item', // required: css class
             'limit' => 4, // the maximum times, an element can be cloned (default 999)
-            'min' => 1, // 0 or 1 (default 1)
+            'min' => "0", // 0 or 1 (default 1)
             'insertButton' => '.add-item', // css class
             'deleteButton' => '.remove-item', // css class
             'model' => $modelsEducationalAttainment[0],
@@ -27,41 +24,47 @@ $year = range(1990, date("Y"));
             ],
         ]); ?>
 
-
-        <div class="container-items">
-            <!-- widgetContainer -->
-            <?php foreach ($modelsEducationalAttainment as $index => $modelEduAt) : ?>
-                <div class="item panel panel-default">
-                    <!-- widgetBody -->
-                    <div class="panel-heading">
-                        <div class="pull-right">
-                            <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
-                            <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-envelope"></i> <?= Yii::t('app', 'Educational_Attainment') ?>
+                <button type="button" class="pull-right add-item btn btn-success btn-xs"><i class="fa fa-plus"></i><?= Yii::t('app', 'Add') ?> </button>
+                <div class="clearfix"></div>
+            </div>
+            <div class="panel-body container-items">
+                <!-- widgetContainer -->
+                <?php foreach ($modelsEducationalAttainment as $index => $modelEduAt) : ?>
+                    <div class="item panel panel-default">
+                        <!-- widgetBody -->
+                        <div class="panel-heading">
+                            <span class="panel-title-address"></span>
+                            <button type="button" class="pull-right remove-item btn btn-danger btn-xs"><i class="fa fa-minus"></i></button>
+                            <div class="clearfix"></div>
                         </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="panel-body">
-                        <?php
-                        // necessary for update action.
-                        if (!$modelEduAt->isNewRecord) {
-                            echo Html::activeHiddenInput($modelEduAt, "[{$index}]id");
-                        }
-                        ?>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <?= $form->field($modelEduAt, "[{$index}]specialization")->textInput(['maxlength' => true]) ?>
-                            </div>
-                            <div class="col-md-4">
-                                <?= $form->field($modelEduAt, "[{$index}]university")->textInput(['maxlength' => true]) ?>
-                            </div>
-                            <div class="col-md-4">
-                                <?= $form->field($modelEduAt, "[{$index}]year_get")->dropDownList($year, ['prompt' => Yii::t('app', 'Plz_Select_Year')])?>
-                            </div>
-                        </div>
+                        <div class="panel-body">
+                            <?php
+                            // necessary for update action.
+                            if (!$modelEduAt->isNewRecord) {
+                                echo Html::activeHiddenInput($modelEduAt, "[{$index}]id");
+                            }
+                            ?>
 
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <?= $form->field($modelEduAt, "[{$index}]specialization")->textInput(['maxlength' => true]) ?>
+                                </div>
+                                <div class="col-sm-4">
+                                    <?= $form->field($modelEduAt, "[{$index}]university")->textInput(['maxlength' => true]) ?>
+                                </div>
+                                <div class="col-sm-4">
+                                    <?= $form->field($modelEduAt, "[{$index}]year_get")->textInput(['maxlength' => true]) ?>
+                                </div>
+                            </div><!-- end:row -->
+
+
+                        </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         </div>
         <?php DynamicFormWidget::end(); ?>
     </div>
