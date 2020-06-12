0c505fc64983e2d3af6ca301047ca6888853cb88
hassanhitleap1 <hassanhitleap1@gmail.com>
2020-06-11 17:40:53 -0700

    يس

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name'=>'جرس لتسويق كفاءات',
+    'name'=>'جرس لتسويق الكفاءات',
     'language' => 'ar',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
