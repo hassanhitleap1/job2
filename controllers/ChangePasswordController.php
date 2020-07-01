ad4c8541294e6fb0c97c875b24e929ecc9ede4f7
hassanhitleap1 <hassanhitleap1@gmail.com>
2020-07-01 10:33:31 -0700

    cerate admin

--- controllers/ChangePasswordController.php
+++ controllers/ChangePasswordController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\ChangePassword;
 use Yii;
 
 
--- models/Admin.php
+++ models/Admin.php
@@ -66,7 +66,6 @@ class Admin extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['name', 'phone'], 'string', 'max' => 255],
             [['name', 'phone'], 'required'],
             [[ 'phone'], 'isJordanPhone'],
             [['phone'],'unique','message'=>Yii::t('app','Phone_Already_Exist')],
@@ -85,26 +84,26 @@ class Admin extends \yii\db\ActiveRecord
             if ($this->isNewRecord) {
                 $this->created_at = $today;
                 $this->updated_at = $today;
-
-                $message_zoom= UserMessageZoom::find()->one()->text;
+               
+                $message_zoom= UserMessageZoom::find()->select('text')->column()[0];
                 $msss_zoom_model=new UserMessageZoom();
                 $msss_zoom_model->text=$message_zoom;
-                $msss_zoom_model->user_id=Yii::app()->user->id;
+                $msss_zoom_model->user_id= Yii::$app->user->identity->id;
 
-                $message_merchant= UserMessageMerchant::find()->one()->text;
+                $message_merchant= UserMessageMerchant::find()->select('text')->column()[0];
                 $message_merchant_model=new UserMessageMerchant();
                 $message_merchant_model->text=$message_merchant;
-                $message_merchant_model->user_id=Yii::app()->user->id;
+                $message_merchant_model->user_id= Yii::$app->user->identity->id;
 
-                $message_clarification= UserMessageClarification::find()->one()->text;
+                $message_clarification= UserMessageClarification::find()->select('text')->column()[0];
                 $msss_clarification_model=new UserMessageClarification();
                 $msss_clarification_model->text=$message_clarification;
-                $msss_clarification_model->user_id=Yii::app()->user->id;
+                $msss_clarification_model->user_id= Yii::$app->user->identity->id;
 
-                $message_user= UserMessage::find()->one()->text;
+                $message_user= UserMessage::find()->select('text')->column()[0];
                 $message_user_model=new UserMessage();
                 $message_user_model->text=$message_user;
-                $message_user_model->user_id=Yii::app()->user->id;
+                $message_user_model->user_id= Yii::$app->user->identity->id;
 
                 $msss_zoom_model->save(false);
                 $message_merchant_model->save(false);
