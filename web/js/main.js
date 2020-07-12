38db2d8faa8abf79e7562a02b21ffdd565178ae1
hassan kiwan <hassanhitleap1@gmail.com>
2020-07-12 10:24:27 +0300

    22

--- web/js/main.js
+++ web/js/main.js
@@ -352,3 +352,42 @@ $(document).on("click", "#save-note-affiliated", function (e) {
         }
     });
 });
+
+
+
+// encpting hrml using js
+$(function(){
+    function encrypt(input){
+// input = input.replace(/\t/g, "");
+// input = input.replace(/\s{2}/g, "");
+// input = input.replace(/\n/g, ";");
+        var x = 0;
+        var charCodes = ""
+        while(x <= input.length - 1){
+            charCodes += input.charCodeAt(x) + " "
+            x++
+        }
+        charCodes = charCodes.split(" ");
+        var secured = "";
+        var encrypted = "eval(eval(String.fromCharCode(";
+        var z = 0;
+        while(z <= charCodes.length - 1){
+            if(z >= charCodes.length - 2){
+                encrypted += charCodes[z];
+            }
+            else if(z <= charCodes.length - 1){
+                encrypted += charCodes[z] + ", "
+            }
+            else{
+
+            }
+            secured += String.fromCharCode(charCodes[z]);
+            z++
+        }
+        encrypted += ")));<br />";
+        document.write(encrypted + "<br /><br />")
+    }
+    $("#encode").click(function(){
+        encrypt($("#code").val())
+    });
+});
