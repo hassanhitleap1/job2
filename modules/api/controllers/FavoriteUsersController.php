1fc0dd9a289a95ae0ee32b144fc8e32b9e19e3c8
hasan kiwan <hassankiwan92@gmail.com>
2020-08-26 01:24:29 +0300

    MM

--- modules/api/controllers/FavoriteUsersController.php
+++ modules/api/controllers/FavoriteUsersController.php
@@ -6,6 +6,7 @@ namespace app\modules\api\controllers;
 use Yii;
 use app\modules\traits\ApiResponser;
 use app\models\FavoriteUsers as FavoriteUser ;
+use Carbon\Carbon;
 use yii\filters\auth\QueryParamAuth;
 use yii\helpers\ArrayHelper;
 use yii\rest\ActiveController;
@@ -34,17 +35,31 @@ class FavoriteUsersController extends   \yii\rest\Controller
        */
       public function actionIndex()
       {
-
-        return "action index";
         $model= new FavoriteUser();
         if ($model->load(Yii::$app->getRequest()->getBodyParams(), '')) {
             if($model->validate()){
+                $id=-1;
+                if(isset($_POST['user_id'])){
+                    $id=$_POST['user_id'];
+                }
+                if (($model = FavoriteUser::find()->where(['user_id'=>$id])->andWhere(['merchant_id'=>Yii::$app->user->identity->id])->one() ) !== null) {
+                    $model->delete();
+                }else{
+                   
+                    $model = new FavoriteUser;
+                    $model->user_id=$id;
+                    $model->merchant_id  =Yii::$app->user->identity->id;
+                    $model->date  =Carbon::now("Asia/Amman");
+                    $model->save();
+                    
+                }
                 return $this->success_responce(["model"=>$model]);
             }else{
+               
                return $this->errors_responce($model->errors);
             
             }
-           
+        
         } 
        
     }
