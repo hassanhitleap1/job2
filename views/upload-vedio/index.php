80449c337ab000311afce68cc5acf1de265dc144
hasan kiwan <hassankiwan92@gmail.com>
2020-11-28 03:04:38 +0200

    mm

--- views/upload-vedio/index.php
+++ views/upload-vedio/index.php
@@ -1,10 +1,9 @@
 <?php
 
-use app\models\NameOfJobs;
+
 use app\models\VedioUser;
 use kartik\file\FileInput;
-use kartik\select2\Select2;
-use yii\helpers\ArrayHelper;
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -64,12 +63,8 @@ if (!$model->isNewRecord) {
             <div class="row">
                 <div class="col-md-12 col-lg-12">
                 <?=  $form->field($model, 'status')->hiddenInput(['value'=> VedioUser::ACTIVE])->label(false); ?>
-                    <?= $form->field($model, 'name_of_jobs_id')->widget(Select2::classname(), [
-                        'data' =>  ArrayHelper::map(NameOfJobs::find()->all(), 'id', 'name_ar'),
-                        'language' => 'ar',
-                        'options' => ['placeholder' =>Yii::t('app',"Plz_Select_Name_Of_Jobs")],
-                       
-                    ]); ?>
+                <?=  $form->field($model, 'name_of_jobs_id')->hiddenInput(['value'=> -1])->label(false); ?>
+                 
                 </div>
                 <div class="col-md-12 col-lg-12">
                     <?=
@@ -97,9 +92,4 @@ if (!$model->isNewRecord) {
         </div>
 
     </div>
-    <div class="container">
-    <div class="row">
-        <iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="existing-iframe-example" width="50%" height="360" src="https://www.youtube.com/embed/ZRsEYJNsMmE?autoplay=1&enablejsapi=1" allow="accelerometer; encrypted-media; 
-            gyroscope; picture-in-picture" frameborder="0" style="border: solid 4px #37474F"></iframe>
-    </div>
-</div>
+   
